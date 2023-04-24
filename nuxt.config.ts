94461b7996690fae2642832b9530e0a788f1d62e
Taha-Abdelmonim <taha.abdelmoonim@gmail.com>
2023-04-24 10:09:37 +0200

    push 2

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -38,6 +38,7 @@ export default defineNuxtConfig({
         { name: "msapplication-TileColor", content: "#ffffff" },
         { name: "msapplication-TileImage", content: "/favicon/ms-icon-144x144.png" },
         { name: "theme-color", content: "#ffffff" },
+        { property: "og:title", hid: "og:title", content: "og title" },
       ],
       script: [
       ]
