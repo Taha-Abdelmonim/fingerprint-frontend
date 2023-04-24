ad20ef86f18950be93861be94b8ce9c9fcbda5ef
Taha-Abdelmonim <taha.abdelmoonim@gmail.com>
2023-04-24 11:33:59 +0200

    push 9

--- pages/blog/[slug].vue
+++ pages/blog/[slug].vue
@@ -104,6 +104,8 @@
     <div class="sharethis-sticky-share-buttons"></div>
   </article>
 </template>
+
+
 <script setup>
 
 import {useTostStore} from "@/store/TostStore";
@@ -135,7 +137,7 @@ const getPost = async () => {
       /* useHead({
         script: [{ type: "text/javascript", src: "https://platform-api.sharethis.com/js/sharethis.js#property=644546f57ac381001a304474&product=sticky-share-buttons&source=platform", async: "async"}],
       }); */
-      useHead({
+      /* useHead({
         script: [{ type: "text/javascript", src: "https://platform-api.sharethis.com/js/sharethis.js#property=644546f57ac381001a304474&product=sticky-share-buttons&source=platform", async: "async"}],
         title: currentLocale.value == "ar" ? res.name_ar : res.name_en,
         meta: [
@@ -163,12 +165,14 @@ const getPost = async () => {
           {property: "twitter:alt", content: currentLocale.value == 'ar' ? res.name_ar : res.name_en},
         ],
         
-      });
+      }); */
     });
   } catch (error) {
     console.log(error);
   }
 };
+
+
 onBeforeMount(async () => {
   await getPost()
 })
@@ -227,6 +231,52 @@ const commentPost = async () => {
 
 };
 
+</script>
+<script>
+
+export default {
+  beforeRouteEnter(to, from, next) {
+    let data = fetch(`https://backend.fingerprintm.com/api/post/${to.params.slug}`, {
+      method: "POST",
+      
+    }).then(res => {
+      res.json().then((result) => {
+        let meta = document.createElement("meta");
+        meta.setAttribute("property", "og:title");
+        meta.content = result.data.name_ar;
+        document.head.prepend(meta)
+        console.log(meta);
+        return result.data;
+      });
+      next(true)
+    });
+  },  
+  
+}
+/* meta: [
+              {name: "title", content:  result.name_ar},
+              {name: "description", content: result.description_ar},
+              // facebook   
+              {property: "article:publisher", content: "https://www.facebook.com/fingerprintmedia1"},
+              {property: "og:locale", content: "ar"},
+              {property: "og:type", content: "article"},
+              {property: "og:url", content: `https://backend.fingerprintm.com/blog/${result.slug}`},
+              {property: "og:title", content:  result.name_en},
+              {property: "og:description", content: result.description_ar},
+              {property: "og:image", content: `https://backend.fingerprintm.com/images/${result.photo}`},
+              {property: "og:site_name", content: "بصمة"},
+              {property: "og:alt", content:  result.name_en},
+              // Twitter
+              {property: "twitter:card", content: "summary_large_image"},
+              {property: "twitter:url", content: `https://backend.fingerprintm.com/blog/${result.slug}`},
+              {property: "twitter:title", content:  result.name_en},
+              {property: "twitter:description", content: result.description_ar},
+              {property: "twitter:type", content: "article"},
+              {property: "twitter:image", content: `https://backend.fingerprintm.com/images/${result.slug}`},
+              {property: "twitter:site", content: "بصمة"},
+              {property: "twitter:site_name", content: "بصمة"},
+              {property: "twitter:alt", content:  result.name_en},
+            ]; */
 </script>
 <style>
 .blog-slug .bread-crumbs {
